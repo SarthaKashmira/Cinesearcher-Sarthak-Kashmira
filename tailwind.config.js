e086b58b040a2dd86c7fcb61e65ff23696f8ad79
BigBinary <bigbinary@Sarthaks-Macbook-Pro.local>
2025-02-10 17:25:19 +0530

    View History Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'viewHistory': '#E4E4F8',
+      'selectViewHistory': '#2f62ff',
+    }),
     extend: {},
   },
   variants: {
