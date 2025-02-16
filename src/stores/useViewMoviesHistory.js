8dd60ce4f634b53b047121f4a93aba57d29813ce
BigBinary <bigbinary@Sarthaks-Macbook-Pro.local>
2025-02-17 02:00:57 +0530

    Added clear movie functionality

--- src/stores/useViewMoviesHistory.js
+++ src/stores/useViewMoviesHistory.js
@@ -12,5 +12,24 @@ const useViewMoviesHistory = create(set => ({
 
       return { moviesHistory, recentMovie: movie };
     }),
+  setRemoveOneMovie: movie =>
+    set(({ moviesHistory, recentMovie }) => {
+      // Remove the movie from moviesHistory
+      const updatedMoviesHistory = moviesHistory.filter(
+        movieItem => movie.imdbID !== movieItem.imdbID
+      );
+
+      // Update recentMovie if it was the removed movie
+      const updatedRecentMovie =
+        recentMovie.imdbID === movie.imdbID && updatedMoviesHistory.length > 0
+          ? updatedMoviesHistory[0]
+          : recentMovie;
+
+      return {
+        moviesHistory: updatedMoviesHistory,
+        recentMovie: updatedRecentMovie,
+      };
+    }),
+  setRemoveAllMovies: () => set(() => ({ moviesHistory: [], recentMovie: {} })),
 }));
 export default useViewMoviesHistory;
