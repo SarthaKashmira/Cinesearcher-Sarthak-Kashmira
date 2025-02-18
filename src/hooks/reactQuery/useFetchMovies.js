0e99cd266bf2480016b540c1571e4bf0b36222c8
BigBinary <bigbinary@Sarthaks-Macbook-Pro.local>
2025-02-18 09:47:31 +0530

    Added enabled in react query hook

--- src/hooks/reactQuery/useFetchMovies.js
+++ src/hooks/reactQuery/useFetchMovies.js
@@ -9,6 +9,7 @@ export const useFetchMovies = ({ searchTerm: s, year: y, page, type }) =>
     queryKey: [QUERY_KEYS.MOVIES, s, y, page, type],
     queryFn: () => movieApi.fetch({ s, y, page, type }),
     keepPreviousData: true, // to keep the previous data
+    enabled: !!s,
     onSuccess: data => {
       if (data?.Response === "False" && data?.Error !== "Incorrect IMDb ID.") {
         Toastr.error(data.Error, { autoClose: 2000 });
